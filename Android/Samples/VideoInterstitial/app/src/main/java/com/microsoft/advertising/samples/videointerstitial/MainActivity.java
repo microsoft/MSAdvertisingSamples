63dee070467568509be986acd9ea314e31ea647d
mukeshagmsft <38921815+mukeshagmsft@users.noreply.github.com>
2018-05-08 11:46:36 +0530

    Update MainActivity.java
    Setting the video interstitial to full screen

--- Android/Samples/VideoInterstitial/app/src/main/java/com/microsoft/advertising/samples/videointerstitial/MainActivity.java
+++ Android/Samples/VideoInterstitial/app/src/main/java/com/microsoft/advertising/samples/videointerstitial/MainActivity.java
@@ -19,9 +19,13 @@ public class MainActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
